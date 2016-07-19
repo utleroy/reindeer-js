29cfd7e93cda0884f5d32d4d3b72e1337e95ebbf
Bryan Hankins <bryanhankins@me.com>
2016-07-19 17:33:31 -0500

    fixed console log

--- reindeer.js
+++ reindeer.js
@@ -5,7 +5,7 @@ var height = ["5'9", "5'10", "5'1", "5'7", "5'9", "6'3", "6'10", "7'2"];
 var hohohoElement = document.getElementById("reindeer");
 
 for (var i = 0; i < reindeer.length; i++) {
-  console.log(reindeer[i] + " " + colors[i] + " " + height[i]); /*[i] runs the arrays through the loop*/
+  console.log(colors[i] + " " + reindeer[i] + " " + height[i]); /*[i] runs the arrays through the loop*/
   hohohoElement.innerHTML += "<div>" + colors[i] + " " + reindeer[i] + " " + height[i] + "</div>";
 }
 
